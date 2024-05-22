0f301b09cf2e9f8040713603ecff8d255cdf13ba
devnoom <archilmenabde@yahoo.com>
2024-05-22 15:25:55 +0400

    Initial Commit

---
+++ First_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  First_SwiftUI
+//
+//  Created by MacBook Air on 22.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ First_SwiftUI/First_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_SwiftUIApp.swift
+//  First_SwiftUI
+//
+//  Created by MacBook Air on 22.05.24.
+//
+
+import SwiftUI
+
+@main
+struct First_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
